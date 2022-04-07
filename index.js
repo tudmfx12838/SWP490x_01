bd136165db29f2331171c5f8a3288809790cd55c
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-04-07 15:41:57 +0900

    1.1 fixed config react and express JS

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
- 
-const app = express()
- 
+const express = require('express');
+const app = express();
+
 app.get('/', (req, res) => {
    res.send('hello from server!')
 })
- 
-app.listen(5000, () => {
-   console.log('App listening on port 5000')
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'I am a message from Server!'});
 })
+
+app.listen(4000, () => console.log('App listening on port 4000'));
