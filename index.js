e9b7e6d9e367a7e47c7f83cb725a630e98fbb40d
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-07-20 00:29:27 +0900

    1.0 backend upload heroku

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ app.use(errorController.getPageError);
 mongoose
   .connect(MONGODB_URL, { dbName: "myShopDB" })
   .then((result) => {
-    // app.listen(4000);
+    app.listen(4000);
     // console.log("App listening on port 4000");
     app.listen(process.env.PORT || 8080, '0.0.0.0', () => {
       console.log('Server is running');
