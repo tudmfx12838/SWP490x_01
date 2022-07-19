58b207265c1dfcb05847da3728f86133296ebdb8
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-07-19 23:56:26 +0900

    created new branch as frontend

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const express = require('express');
-const router = express.Router();
-
-const productController = require('../controllers/product');
-
-// router.get('admin/products',productController.getProducts);
-
-module.exports = router;
