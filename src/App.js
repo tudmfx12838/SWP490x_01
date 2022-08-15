8d4efe2c9182b9bb99d9989589d81b34938a2204
Dang Minh Tu FX12838 <tudmfx12838@funix.edu.vn>
2022-08-15 22:18:19 +0900

    Added google analysis

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, { Component } from "react";
+import React, {useEffect }from "react";
 import Main from "./components/MainComponent";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { ConfigureStore } from './redux/configureStore';
+import ReactGA from 'react-ga';
+const TRACKING_ID = "G-M8Q7W3FESF"; 
+
+ReactGA.initialize(TRACKING_ID);
 
 const store = ConfigureStore();
 
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <Main />
-        </BrowserRouter>
-      </Provider>
-    );
-  }
+const App = () => {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+ 
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+    </Provider>
+  );
+  
 }
 
 export default App;
